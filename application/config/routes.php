a9dfed86dcf6c8883ccfacddeb6563244706f3a2
MechFroG88 <kelzzin2@gmail.com>
2019-05-28 14:58:08 +0800

    Update Route

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,7 +10,7 @@ $route['translate_uri_dashes'] = FALSE;
  * Users  
  */
 $route['user']['POST']                       = 'User/create';
-$route['login']['POST']                      = 'User/login';
+$route['user/login']['POST']                 = 'User/login';
 $route['user']['GET']                        = 'User/get';
-$route['logout']['GET']                      = 'User/logout';
+$route['user/logout']['POST']                = 'User/logout';
 $route['user/(:num)']['DELETE']              = 'User/delete/$1';
